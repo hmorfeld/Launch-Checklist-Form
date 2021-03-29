d37871c7f21881257c1f4ceb51f26ca0fde647c0
hmorfeld <79673992+hmorfeld@users.noreply.github.com>
2021-03-29 17:32:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
-// Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.addEventListener("load", function(){
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      return response.json();
+         }).then(function(json){
+      console.log(json);
+      let missionTarget = document.querySelector("#missionTarget");
+      <div id="destination">
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${destination.name}</li>
+               <li>Diameter: ${destination.diameter}</li>
+               <li>Star: ${destination.star}</li>
+               <li>Distance from Earth: ${destination.distance}</li>
+               <li>Number of Moons: ${destination.moons}</li>
+            </ol>
+            <img src="${destination.image}"></img>
+        </div> 
+   })
+   let form = document.getElementById("launchForm");
+   form.addEventListener("submit",function(e){
+      e.preventDefault();
+
+      
+      let button = document.getElementById("formSubmit");
+      let name = document.getElementById("pilotName");
+      let coName = document.getElementById("copilotName");
+      let fuelLevel = document.getElementById("fuelLevel");
+      let mass = document.getElementById("cargoMass");
+
+      if (name.value === ""){
+         window.alert("Your Forgot Your Name!")
+      }
+      if (coName.value === ""){
+         window.alert("Do you have a Co-Pilot? Adde their name!");
+      }
+      if( fuelLevel.value === ""){
+         window.alert("Add Fuel Level!");
+      }
+      if (mass.value === ""){
+         window.alert("Add Cargo Mass!")
+      }
+      if (isNaN(fuelLevel.value)){
+         window.alert("Please Enter a Number for Fuel Level!")
+      }
+      if(isNaN(mass.value)){
+         window.alert("Please Enter a Number for Cargo Mass!")
+      }
+      if (!isNaN(name.value)) {
+         window.alert("Please use only letters for Names.");
+         console.log(typeof(name.value))
+      }
+      if (!isNaN(coName.value)){
+         window.alert("Please use only numbers for mass!")
+      }
+
+   });
+})
