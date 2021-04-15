f02ada30f2ca170b9e3f0ce4c3771c129c5bffa3
hmorfeld <hmorfeld91@gmail.com>
2021-04-14 23:57:58 -0500

    triend fetching and adding submit code

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ window.addEventListener("load", function(){
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
       return response.json();
          }).then(function(json){
-      console.log(json);
-      let missionTarget = document.querySelector("#missionTarget");
-      <div id="destination">
-            <h2>Mission Destination</h2>
-            <ol>
-               <li>Name: ${destination.name}</li>
-               <li>Diameter: ${destination.diameter}</li>
-               <li>Star: ${destination.star}</li>
-               <li>Distance from Earth: ${destination.distance}</li>
-               <li>Number of Moons: ${destination.moons}</li>
-            </ol>
-            <img src="${destination.image}"></img>
-        </div> 
-   })
+            console.log(json);
+            let container = document.querySelector('#missionTarget');
+            let targetPlanet = '';
+            for (planet in json){
+               targetPlanet =`
+               <div id="missionTarget">
+                  <div id= "destination">
+                     <h2>Mission Target</h2>
+                     <ol>
+                        <li>Name: ${planet.name}</li>
+                        <li>Diameter: ${planet.diameter}</li>
+                        <li>Star: ${planet.star}</li>
+                        <li>Distance from Earth: ${planet.distance}</li>
+                        <li>Number of Moons: ${planet.moons}</li>
+                     </ol>
+                     <img src="${planet.image}"></img>
+                  </div>
+               </div>
+               `
+      }
+      container.innerHTML = targetPlanet
+
+   
+      
    let form = document.getElementById("launchForm");
    form.addEventListener("submit",function(e){
       e.preventDefault();
 
-      
       let button = document.getElementById("formSubmit");
       let name = document.getElementById("pilotName");
       let coName = document.getElementById("copilotName");
@@ -29,7 +38,8 @@ window.addEventListener("load", function(){
       let mass = document.getElementById("cargoMass");
 
       if (name.value === ""){
-         window.alert("Your Forgot Your Name!")
+         window.alert("Your Forgot Your Name!");
+         console.log("This is working")
       }
       if (coName.value === ""){
          window.alert("Do you have a Co-Pilot? Adde their name!");
@@ -40,5 +50,7 @@ window.addEventListener("load", function(){
       if (mass.value === ""){
          window.alert("Add Cargo Mass!");
       };
-   })
-})    
+   });
+})
+
+})
